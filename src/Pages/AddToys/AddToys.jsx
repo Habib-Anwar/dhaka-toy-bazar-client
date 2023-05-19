45b6e47b18f7dd0374b7aa45eac530d6d8f06d78
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-05-20 04:27:53 +0600

    add a toy

---
+++ src/Pages/AddToys/AddToys.jsx
@@ -0,0 +1,11 @@
+
+
+const AddToys = () => {
+    return (
+        <div>
+            <h2>Add A Toy:</h2>
+        </div>
+    );
+};
+
+export default AddToys;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home/Home";
 import Blog from "../Pages/Blog/Blog";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import AddToys from "../Pages/AddToys/AddToys";
 
 const router = createBrowserRouter([
     {
@@ -25,6 +26,11 @@ const router = createBrowserRouter([
         {
           path: "signup",
           element: <SignUp></SignUp>
+        },
+        {
+          path: "addtoys/:id",
+          element: <AddToys></AddToys>,
+          loader: ({params}) => fetch(`http://localhost:5000/bazar/${params.id}`)
         }
       ]
     },
